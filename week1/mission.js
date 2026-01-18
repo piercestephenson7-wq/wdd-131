53152c024650967fe751047f8319c0e7d3ad57d3
piercestephenson7-wq <piercestephenson7@gmail.com>
2026-01-17 17:17:31 -0700

    1/17

---
+++ week1/mission.js
@@ -0,0 +1,18 @@
+                    
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        document.body.style.backgroundColor = '#333';
+        document.body.style.color = 'white';
+        logo.src = 'byui-logo-dark.png';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+        logo.src = 'byui-logo-blue.webp';
+    }
+}  
