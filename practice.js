11d8791c3f242f639986ca70b1bbcae0230a3b4d
piercestephenson7-wq <piercestephenson7@gmail.com>
2026-01-29 09:59:35 -0700

    1/29

---
+++ practice.js
@@ -0,0 +1,59 @@
+function fruitColor(fruit) {
+    let color = "unknown";
+    switch (fruit.toLowerCase()) {
+        case 'apple':
+        case 'cherry':
+        case 'strawberry':
+            color = 'red';
+            break;
+        case 'banana':
+        case 'lemon':
+            color = 'yellow';
+            break;
+        case 'kiwi':
+        case 'lime':
+            color = 'green';
+            break;
+        case 'blueberry':
+        case 'grape':
+            color = 'purple';
+            break;
+        default:
+            color = 'unknown';
+    }
+    return color;
+}
+
+
+function door(state) {
+    let result = "unknown";
+
+    switch (state) {
+        case 0:
+            result = "closed";
+            break;
+        case 1:
+            result = "open";
+            break;
+        default:
+            result = "unknown";
+    }
+    return result;
+}
+
+// door function but different
+
+function doorStatus(state) {
+    switch (state) {
+        case 0:
+            return "closed";
+        case 1:
+            return "open";
+        default:
+            return "unknown";  
+    }
+}
+
+state = prompt("Enter door state (0 for closed, 1 for open):");
+doorStatus(state);
+alert("The door is " + doorStatus(parseInt(state)));
