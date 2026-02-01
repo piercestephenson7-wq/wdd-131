3da35cb0614934411454022f45f52848c3fbd66f
piercestephenson7-wq <piercestephenson7@gmail.com>
2026-01-31 23:34:12 -0700

    1/31

--- week3/coolpics/index.js
+++ week3/coolpics/index.js
@@ -13,3 +13,30 @@ document.addEventListener('click', (e) => {
         menuBtn.classList.remove('open');
     }
 });
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(evt) {
+    if (evt.target.tagName === 'IMG') {
+        modalImage.src = evt.target.dataset.large || evt.target.src;
+        modal.showModal();
+    }   
+}
+
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
