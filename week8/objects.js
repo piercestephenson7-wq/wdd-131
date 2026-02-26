7bf6fbf5711d11a905e1ddd92468c24638b3efb3
piercestephenson7-wq <piercestephenson7@gmail.com>
2026-02-26 09:19:05 -0700

    Course enrollment

--- week8/objects.js
+++ week8/objects.js
@@ -4,12 +4,12 @@
         name: 'Javascript Language',
         logo: 'images/js-logo.png',
         sections: [
-        { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro A'},
-        { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis B'},
-        { sectionNum: 3, roomNum: 'STC 231', enrolled: 10, days: 'MWF', instructor: 'Bro C'},
-        { sectionNum: 4, roomNum: 'STC 101', enrolled: 15, days: 'MWF', instructor: 'Sis D'},
-        { sectionNum: 5, roomNum: 'STC 102', enrolled: 20, days: 'MWF', instructor: 'Bro E'},
-        { sectionNum: 6, roomNum: 'STC 103', enrolled: 18, days: 'TTh', instructor: 'Sis F'},
+        { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro Awesome'},
+        { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis Beast'},
+        { sectionNum: 3, roomNum: 'STC 231', enrolled: 10, days: 'MWF', instructor: 'Bro Cool'},
+        { sectionNum: 4, roomNum: 'STC 398', enrolled: 15, days: 'MWF', instructor: 'Sis Dope'},
+        { sectionNum: 5, roomNum: 'STC 394', enrolled: 20, days: 'MWF', instructor: 'Bro Epic'},
+        { sectionNum: 6, roomNum: 'STC 245', enrolled: 18, days: 'TTh', instructor: 'Sis Fantastic'},
         ],
         enrollStudent: function (sectionNum) {
             // find the right section...Array.findIndex will work here
@@ -45,5 +45,27 @@
     document.querySelector("#enrollStudent").addEventListener("click", function () {
         const sectionNum = document.querySelector("#sectionNumber").value;
         aCourse.enrollStudent(sectionNum);
+    });
+
+    const addSectionButton = document.querySelector("#addSection");
+    addSectionButton.addEventListener("click", function () {
+        const newSectionNum = aCourse.sections.length + 1;
+        const newRoomNum = document.querySelector("#newRoomNumber").value;
+        const newEnrolled = parseInt(document.querySelector("#newEnrolled").value);
+        const newDays = document.querySelector("#newDays").value;
+        const newInstructor = document.querySelector("#newInstructor").value;
+
+        const newSection = {
+            sectionNum: newSectionNum,
+            roomNum: newRoomNum,
+            enrolled: newEnrolled,
+            days: newDays,
+            instructor: newInstructor
+        };
+
+        aCourse.sections.push(newSection);
+        renderSections(aCourse.sections);
+        const clearFields = document.querySelectorAll(".newSectionInput");
+        clearFields.forEach(field => field.value = "");
     });
           
