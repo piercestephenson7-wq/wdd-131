aea4731034308cbc66f76b4e2177894dfa3996e3
piercestephenson7-wq <piercestephenson7@gmail.com>
2026-03-17 10:19:40 -0600

    final project

--- Personal_Site/index.js
+++ Personal_Site/index.js
@@ -24,25 +24,59 @@ const prevBtn = document.getElementById('prev');
 
 let index = 0;
 
-nextBtn.addEventListener('click', () => {
+function moveToNext() {
   const cards = document.querySelectorAll('.card');
-  if (index < cards.length - 1) {
-    index++;
-  } else {
-    index = 0; 
-  }
+  // Use cards.length so it works for any number of services
+  index = (index < cards.length - 1) ? index + 1 : 0;
   updatePosition();
+}
+
+function moveToPrev() {
+  const cards = document.querySelectorAll('.card');
+  // Instead of hard-coding '2', use cards.length - 1
+  index = (index > 0) ? index - 1 : cards.length - 1;
+  updatePosition();
+}
+
+let autoSpin = setInterval(moveToNext, 3000); // Spins every 3 seconds
+
+function resetTimer() {
+  clearInterval(autoSpin);
+  autoSpin = setInterval(moveToNext, 3000);
+}
+
+// Update your existing button listeners
+nextBtn.addEventListener('click', () => {
+  moveToNext();
+  resetTimer();
 });
 
 prevBtn.addEventListener('click', () => {
-  if (index > 0) {
-    index--;
-  } else {
-    index = 2;
-  }
-  updatePosition();
+  moveToPrev();
+  resetTimer();
 });
 
+// Add the new touch listeners here
+let touchStartX = 0;
+let touchEndX = 0;
+
+track.addEventListener('touchstart', (e) => {
+  touchStartX = e.changedTouches[0].screenX;
+  clearInterval(autoSpin); 
+}, { passive: true });
+
+track.addEventListener('touchend', (e) => {
+  touchEndX = e.changedTouches[0].screenX;
+  if (touchStartX - touchEndX > 50) moveToNext();
+  else if (touchEndX - touchStartX > 50) moveToPrev();
+  resetTimer();
+}, { passive: true });
+
 function updatePosition() {
-  track.style.transform = `translateX(-${index * 300}px)`;
+  const cards = document.querySelectorAll('.card');
+  if (cards.length > 0) {
+    // Uses the actual width of the card in case you change CSS later
+    const width = cards[0].offsetWidth; 
+    track.style.transform = `translateX(-${index * width}px)`;
+  }
 }
